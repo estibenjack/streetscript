d53736b6efcbbef49932d96f87b6d7331d9f0702
Steven <156148848+estibenjack@users.noreply.github.com>
2026-03-24 20:33:01 +0000

    add POST endpoints for decks and cards

--- decks/views.py
+++ decks/views.py
@@ -1,32 +1,50 @@
 from rest_framework.decorators import api_view
-# from rest_framework.response import Response
+from rest_framework.response import Response
+from rest_framework import status
 from django.http import JsonResponse
 # from .sample_data import SAMPLE_DECKS, SAMPLE_CARDS
 from .models import Deck, Card
 from .serializers import DeckSerializer, CardSerializer
 
-@api_view(['GET'])
-def get_decks(request):
-    decks = Deck.objects.all()
-    serializer = DeckSerializer(decks, many=True)
-    return JsonResponse(serializer.data, safe=False)
+
+@api_view(['GET', 'POST'])
+def decks_list(request):
+    if request.method == 'GET':
+        decks = Deck.objects.all()
+        serializer = DeckSerializer(decks, many=True)
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        # pass the request body to the serializer here
+        serializer = DeckSerializer(data=request.data)
+        # validate data against model's field rules
+        if serializer.is_valid():
+            #write to db (django handles INSERT)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 
 @api_view(['GET'])
-def get_deck(request, deck_id):
+def deck_detail(request, deck_id):
     try:
         deck = Deck.objects.get(id=deck_id)
     except Deck.DoesNotExist:
-        return JsonResponse({'error': 'Deck not found'}, status=404)
+        return Response({'error': 'Deck not found'}, status=status.HTTP_404_NOT_FOUND)
     serializer = DeckSerializer(deck)
-    return JsonResponse(serializer.data, safe=False)
-    '''
     return Response(serializer.data)
-    if using Response instead of JsonResponse, add safe=False since it's expecting a 
-  
-    '''
 
-@api_view(['GET'])
-def get_cards_for_deck(request, deck_id):
-    cards = Card.objects.filter(deck_id=deck_id)
-    serializer = CardSerializer(cards, many=True)
-    return JsonResponse(serializer.data, safe=False)
+
+@api_view(['GET', 'POST'])
+def cards_for_deck(request, deck_id):
+    if request.method == 'GET':
+        cards = Card.objects.filter(deck_id=deck_id)
+        serializer = CardSerializer(cards, many=True)
+        return Response(serializer.data)
+    
+    if request.method == 'POST':
+        serializer = CardSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
