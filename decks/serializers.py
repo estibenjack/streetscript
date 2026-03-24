b8b45ff25525a7bde4b850bbe42412003bcac7a0
Steven <156148848+estibenjack@users.noreply.github.com>
2026-03-24 16:54:01 +0000

    replace sample data with real db queries

--- decks/serializers.py
+++ decks/serializers.py
@@ -1,14 +1,23 @@
 from rest_framework import serializers
+from .models import Deck, Card
 
-class CardSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    deck_id = serializers.IntegerField()
-    front = serializers.CharField()
-    back = serializers.CharField()
-    context = serializers.CharField()
+'''
+used sample data previously, not using models and postgres db so:
+using serializers.ModelSerializer instead of .Serializer
+--> instead of deining the fields manually, it reads the model and knows
+    the fields already
+Meta class tells it which model to use
 
-class DeckSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    name = serializers.CharField()
-    description = serializers.CharField()
-    language = serializers.CharField()
+- in terms of order, Card depends on Deck in the model bc of foreign key
+-- child before parent: parent serializer will reference the child serializer
+'''
+
+class CardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Card
+        fields = ['id', 'deck', 'front', 'back', 'context', 'created_at']
+        
+class DeckSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Deck
+        fields = ['id', 'name', 'description', 'language', 'created_at']
--- decks/views.py
+++ decks/views.py
@@ -1,29 +1,32 @@
 from rest_framework.decorators import api_view
 # from rest_framework.response import Response
 from django.http import JsonResponse
-from .sample_data import SAMPLE_DECKS, SAMPLE_CARDS
+# from .sample_data import SAMPLE_DECKS, SAMPLE_CARDS
+from .models import Deck, Card
 from .serializers import DeckSerializer, CardSerializer
 
 @api_view(['GET'])
 def get_decks(request):
-    serializer = DeckSerializer(SAMPLE_DECKS, many=True)
+    decks = Deck.objects.all()
+    serializer = DeckSerializer(decks, many=True)
     return JsonResponse(serializer.data, safe=False)
 
 @api_view(['GET'])
 def get_deck(request, deck_id):
-    deck = next((d for d in SAMPLE_DECKS if d['deck_id'] == deck_id), None)
-    if deck is None:
+    try:
+        deck = Deck.objects.get(id=deck_id)
+    except Deck.DoesNotExist:
         return JsonResponse({'error': 'Deck not found'}, status=404)
     serializer = DeckSerializer(deck)
+    return JsonResponse(serializer.data, safe=False)
     '''
     return Response(serializer.data)
     if using Response instead of JsonResponse, add safe=False since it's expecting a 
   
     '''
-    return JsonResponse(serializer.data, safe=False)
 
 @api_view(['GET'])
 def get_cards_for_deck(request, deck_id):
-    cards = [c for c in SAMPLE_CARDS if c['deck_id'] == deck_id]
+    cards = Card.objects.filter(deck_id=deck_id)
     serializer = CardSerializer(cards, many=True)
     return JsonResponse(serializer.data, safe=False)
