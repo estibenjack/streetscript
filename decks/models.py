dbf9ca7a0beedc3f549739f7b9f80f7f45fc5284
Steven <156148848+estibenjack@users.noreply.github.com>
2026-03-24 16:08:38 +0000

    add Deck and Card models with initial migration

--- decks/models.py
+++ decks/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+'''
+- django models inherit from models.Model to get django's orm functionality
+- having CASCADE in the foreign key r/ship means all related cards are deleted
+  from a deck if it's deleted
+- related_name='cards' let's us do deck.cards.all() to get all cards for a deck
+'''
+class Deck(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    language = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Card(models.Model):
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE, related_name='cards')
+    front = models.CharField(max_length=200)
+    back = models.CharField(max_length=200)
+    context = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.front} -> {self.back}"
