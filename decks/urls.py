d9e9bcded1d1878aafcd0a0c96246f256a3da87c
Steven <156148848+estibenjack@users.noreply.github.com>
2026-03-26 04:01:08 +0000

    add full CRUD endpoints for cards including card_detail

--- decks/urls.py
+++ decks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
   path('decks/', views.decks_list, name='decks_list'),
   path('decks/<int:deck_id>/', views.deck_detail, name='deck_detail'),
