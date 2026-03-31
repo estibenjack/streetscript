87204a6ec5c4f4668fa7b67b85bba58f2853ee31
Steven <156148848+estibenjack@users.noreply.github.com>
2026-03-31 01:25:36 +0100

    remove old comments

--- decks/serializers.py
+++ decks/serializers.py
@@ -1,17 +1,6 @@
 from rest_framework import serializers
 from .models import Deck, Card
 
-'''
-used sample data previously, now using models and postgres db so:
-using serializers.ModelSerializer instead of .Serializer
---> instead of defining the fields manually, it reads the model and knows
-    the fields already
-Meta class tells it which model to use
-
-- in terms of order, Card depends on Deck in the model bc of foreign key
--- child before parent: parent serializer will reference the child serializer
-'''
-
 class CardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Card
